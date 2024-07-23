f12a0a01d0ef4d19cf10ca531c91e565b8b794a0
Tecktron <Tecktron@users.noreply.github.com>
2024-07-23 16:10:42 -0400

    updated prestart to add check if psql is installed

--- prestart.sh
+++ prestart.sh
@@ -15,22 +15,31 @@ else
   PGCONNECT=postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/postgres
 fi
 
-until psql $PGCONNECT -c '\q'; do
-  if [ $x -gt 10 ]; then
-    echo "Cannot find postgres, aborting"
-    exit 8
-  fi
-  echo "Postgres is unavailable - sleeping..."
-  sleep 3
-  x=$(($x+1))
-done
-
-echo "Postgres is up!"
-cd /app || exit 1
-
-if [ "${SKIP_CREATE_DB}" != "true" ]; then
-  psql ${PGCONNECT} -tc "SELECT 1 FROM pg_database WHERE datname = '${DB_NAME}'" | grep -q 1 || psql ${PGCONNECT} -c "CREATE DATABASE \"${DB_NAME}\" WITH OWNER '${DB_USER}'"
+PSQL_INSTALLED="true"
+hash psql &> /dev/null
+if [ $? -eq 1 ]; then
+  PSQL_INSTALLED="false"
 fi
+
+if [ "${PSQL_INSTALLED}" = "true" ]; then
+  until psql $PGCONNECT -c '\q'; do
+    if [ $x -gt 10 ]; then
+      echo "Cannot find postgres, aborting"
+      exit 8
+    fi
+    echo "Postgres is unavailable - sleeping..."
+    sleep 3
+    x=$(($x+1))
+  done
+
+  echo "Postgres is up!"
+
+  if [ "${SKIP_CREATE_DB}" != "true" ]; then
+    psql ${PGCONNECT} -tc "SELECT 1 FROM pg_database WHERE datname = '${DB_NAME}'" | grep -q 1 || psql ${PGCONNECT} -c "CREATE DATABASE \"${DB_NAME}\" WITH OWNER '${DB_USER}'"
+  fi
+fi
+
+cd /app || exit 1
 
 ADMIN_USER=${ADMIN_USER:-"admin"}
 ADMIN_PASS=${ADMIN_PASS:-"admin"}
